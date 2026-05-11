51d7e423a47457b162efeaf90a476cab4d613ba8
rub3nnn <rubenmoraleszgz@gmail.com>
2026-05-11 19:18:41 +0200

    feat: add sites router to main API router

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -6,10 +6,12 @@ const { Router } = require("express");
 
 const authRouter = require("./auth/auth.routes");
 const projectsRouter = require('./projects/projects.routes');
+const sitesRouter = require('./sites/sites.routes');
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use('/projects', projectsRouter);
+router.use('/sites', sitesRouter);
 
 module.exports = router;
