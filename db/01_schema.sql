78abed532b7402309f66b0ada5f4741814ad45cf
Diego Puertolas <diegopuertolasruiz@gmail.com>
2026-05-11 17:41:27 +0200

    feat: add created_by column to projects table for user association

--- db/01_schema.sql
+++ db/01_schema.sql
@@ -48,6 +48,7 @@ CREATE TABLE IF NOT EXISTS users (
 -- projects
 CREATE TABLE IF NOT EXISTS projects (
   id          UUID         PRIMARY KEY DEFAULT gen_random_uuid(),
+  created_by  UUID         NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   name        VARCHAR(255) NOT NULL,
   api_key     UUID         NOT NULL UNIQUE DEFAULT gen_random_uuid(),
   description TEXT,
