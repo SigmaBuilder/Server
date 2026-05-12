5a305ab47d6adcaa971c314ceeee2fed66597491
Diego Puertolas <diegopuertolasruiz@gmail.com>
2026-05-12 11:50:13 +0200

    docs: add JSDoc comments to portfolio items controller methods

--- src/api/v1/projects/sites/portfolio_items/portfolio_items.controller.js
+++ src/api/v1/projects/sites/portfolio_items/portfolio_items.controller.js
@@ -6,6 +6,12 @@ const service = require('./portfolio_items.service');
 const { sendSuccess } = require('../../../../../utils/response');
 const HTTP_STATUS = require('../../../../../constants/httpStatus');
 
+/**
+ * Obtiene todos los elementos del portfolio ordenados por 'sort_order'
+ * @param {Object} req - Objeto de petición.
+ * @param {Object} res - Objeto de respuesta.
+ * @param {Function} next - Función para pasar al siguiente middleware.
+ */
 const getAll = async (req, res, next) => {
   try {
     const data = await service.getAll(req.params.siteId);
@@ -15,6 +21,12 @@ const getAll = async (req, res, next) => {
   }
 };
 
+/**
+ * Obtiene un elemento del portfolio por ID.
+ * @param {Object} req - Objeto de petición.
+ * @param {Object} res - Objeto de respuesta.
+ * @param {Function} next - Función para pasar al siguiente middleware.
+ */
 const getById = async (req, res, next) => {
   try {
     const data = await service.getById(req.params.siteId, req.params.id);
@@ -24,6 +36,12 @@ const getById = async (req, res, next) => {
   }
 };
 
+/**
+ * Crea un nuevo elemento del portfolio.
+ * @param {Object} req - Objeto de petición.
+ * @param {Object} res - Objeto de respuesta.
+ * @param {Function} next - Función para pasar al siguiente middleware.
+ */
 const create = async (req, res, next) => {
   try {
     const data = await service.create(req.params.siteId, req.body);
@@ -33,6 +51,12 @@ const create = async (req, res, next) => {
   }
 };
 
+/**
+ * Actualiza un elemento del portfolio.
+ * @param {Object} req - Objeto de petición.
+ * @param {Object} res - Objeto de respuesta.
+ * @param {Function} next - Función para pasar al siguiente middleware.
+ */
 const update = async (req, res, next) => {
   try {
     const data = await service.update(req.params.siteId, req.params.id, req.body);
@@ -42,6 +66,12 @@ const update = async (req, res, next) => {
   }
 };
 
+/**
+ * Elimina un elemento del portfolio.
+ * @param {Object} req - Objeto de petición.
+ * @param {Object} res - Objeto de respuesta.
+ * @param {Function} next - Función para pasar al siguiente middleware.
+ */
 const remove = async (req, res, next) => {
   try {
     await service.remove(req.params.siteId, req.params.id);
