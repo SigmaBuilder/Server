63acfc38c3dccea84d117d2ccceb3b75525d9598
rub3nnn <rubenmoraleszgz@gmail.com>
2026-05-12 17:25:42 +0200

    refactor: remove unused site retrieval functions from sites.controller.js

--- src/api/v1/projects/sites/sites.controller.js
+++ src/api/v1/projects/sites/sites.controller.js
@@ -24,42 +24,6 @@ const getAllSitesByProject = async (req, res, next) => {
 };
 
 /**
- * Obtiene un site por su ID.
- * @param {Object} req - Objeto de petición.
- * @param {Object} res - Objeto de respuesta.
- * @param {Function} next - Función para pasar al siguiente middleware.
- */
-const getSiteById = async (req, res, next) => {
-  try {
-    const site = await sitesService.getSiteById(
-      req.params.projectId,
-      req.params.siteId,
-    );
-    sendSuccess(res, { site });
-  } catch (err) {
-    next(err);
-  }
-};
-
-/**
- * Obtiene un site por su slug.
- * @param {Object} req - Objeto de petición.
- * @param {Object} res - Objeto de respuesta.
- * @param {Function} next - Función para pasar al siguiente middleware.
- */
-const getSiteBySlug = async (req, res, next) => {
-  try {
-    const site = await sitesService.getSiteBySlug(
-      req.params.projectId,
-      req.params.slug,
-    );
-    sendSuccess(res, { site });
-  } catch (err) {
-    next(err);
-  }
-};
-
-/**
  * Crea un nuevo site.
  * @param {Object} req - Objeto de petición.
  * @param {Object} res - Objeto de respuesta.
@@ -74,45 +38,7 @@ const createSite = async (req, res, next) => {
   }
 };
 
-/**
- * Actualiza un site.
- * @param {Object} req - Objeto de petición.
- * @param {Object} res - Objeto de respuesta.
- * @param {Function} next - Función para pasar al siguiente middleware.
- */
-const updateSite = async (req, res, next) => {
-  try {
-    const site = await sitesService.updateSite(
-      req.params.projectId,
-      req.params.siteId,
-      req.body,
-    );
-    sendSuccess(res, { site });
-  } catch (err) {
-    next(err);
-  }
-};
-
-/**
- * Elimina un site.
- * @param {Object} req - Objeto de petición.
- * @param {Object} res - Objeto de respuesta.
- * @param {Function} next - Función para pasar al siguiente middleware.
- */
-const deleteSite = async (req, res, next) => {
-  try {
-    await sitesService.deleteSite(req.params.projectId, req.params.siteId);
-    sendSuccess(res, null, HTTP_STATUS.NO_CONTENT);
-  } catch (err) {
-    next(err);
-  }
-};
-
 module.exports = {
   getAllSitesByProject,
-  getSiteById,
   createSite,
-  updateSite,
-  deleteSite,
-  getSiteBySlug,
 };
