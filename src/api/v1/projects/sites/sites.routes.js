6a73531e56af153bc6fb10749ae03898f8437757
Diego Puertolas <diegopuertolasruiz@gmail.com>
2026-05-12 11:46:01 +0200

    feat: refactor site routes to improve readability and structure

--- src/api/v1/projects/sites/sites.routes.js
+++ src/api/v1/projects/sites/sites.routes.js
@@ -11,25 +11,22 @@ const { createSiteRules, updateSiteRules } = require("./sites.validator");
 const validate = require("../../../../middlewares/validate");
 const authorize = require("../../../../middlewares/authorize");
 
+// Sub-rutas
+const portfolioItemsRouter = require('./portfolio_items/portfolio_items.routes');
+
 const router = Router({ mergeParams: true });
 
 router.get("/", authorize("project:read"), controller.getAllSitesByProject);
 router.get("/slug/:slug", authorize("project:read"), controller.getSiteBySlug);
 router.get("/:siteId", authorize("project:read"), controller.getSiteById);
-router.post(
-  "/",
-  authorize("project:update"),
-  createSiteRules,
-  validate,
-  controller.createSite,
-);
-router.patch(
-  "/:siteId",
-  authorize("project:update"),
-  updateSiteRules,
-  validate,
-  controller.updateSite,
-);
+
+router.post("/", authorize("project:update"), createSiteRules, validate, controller.createSite);
+
+router.patch("/:siteId", authorize("project:update"), updateSiteRules, validate, controller.updateSite);
+
 router.delete("/:siteId", authorize("project:update"), controller.deleteSite);
+
+// Delegar sub-rutas de portfolio_items
+router.use('/:siteId/portfolio_items', portfolioItemsRouter);
 
 module.exports = router;
