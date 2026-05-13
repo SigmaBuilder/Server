9946d5b72140aa3bf77cb7fcb582354412205272
rub3nnn <rubenmoraleszgz@gmail.com>
2026-05-13 18:38:22 +0200

    feat: add invitations router to the main API routes

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -7,11 +7,13 @@ const { Router } = require("express");
 const authRouter = require("./auth/auth.routes");
 const projectsRouter = require('./projects/projects.routes');
 const sitesRouter = require('./sites/sites.routes');
+const invitationsRouter = require('./invitations/invitations.routes');
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use('/projects', projectsRouter);
 router.use('/sites', sitesRouter);
+router.use('/invitations', invitationsRouter);
 
 module.exports = router;
